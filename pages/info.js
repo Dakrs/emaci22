8753889f5969dfcb1ed134ba40f1fa4ef1c057db
Diogo Sobral <diogosobral98@gmail.com>
2022-01-27 13:59:05 +0000

    fix ' error on build

--- pages/info.js
+++ pages/info.js
@@ -21,11 +21,10 @@ export default function Home() {
               </div>
             </div>
             <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto rounded-lg w-full p-8 mt-8 md:mt-0">
-              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Event's Main Website</h2>
+              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">{'Event\'s Main Website'}</h2>
               <a href="https://www.emaci2022braga.com/en" className="leading-relaxed mb-5 text-gray-600">https://www.emaci2022braga.com/en</a>
-              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Event's Duration</h2>
+              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">{'Event\'s Duration'}</h2>
               <a href="https://www.emaci2022braga.com/en" className="leading-relaxed mb-5 text-gray-600">20 - 27 February</a>
-
               <div className="relative mb-4">
                 <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
                 <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
