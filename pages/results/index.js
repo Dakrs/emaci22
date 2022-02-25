a3d2dfe3719de1c6864d07b9c693b2a553cbd85d
Diogo Sobral <diogosobral98@gmail.com>
2022-02-25 09:47:05 +0000

    removed gender from table

--- pages/results/index.js
+++ pages/results/index.js
@@ -40,25 +40,23 @@ const AvailResults = (props) => {
         accessor: 'nome'
     },
     {
-      Header: 'Gender',
-      accessor: 'sexo'
-    },
-    {
       Header: 'Results',
       accessor: 'id',
       Cell: ({value}) => {
         return (
-          <Link href={`/results/multi/${value}`} passHref>
-              <a className="px-6 py-2 text-lg font-semibold text-center rounded text-white bg-slate-700 hover:bg-slate-600">
-                  Results
-              </a>
-           </Link>
+          <div className="w-full flex items-center justify-center">
+            <Link href={`/results/multi/${value}`} passHref>
+                <a className="px-6 py-2 text-lg font-semibold text-center rounded text-white bg-slate-700 hover:bg-slate-600">
+                    Results
+                </a>
+             </Link>
+          </div>
         )
       }
     }
   ]
 
-  
+
   if (data.resultados.length === 0 && data.combinadas.length === 0)
     return (
       <div className="w-full min-h-screen h-full bg-slate-100 flex justify-center items-center">
@@ -109,7 +107,7 @@ const AvailResults = (props) => {
           </div>
 
           <div className="w-full py-4">
-              <div className="container mx-auto">
+              <div className="container mx-auto max-w-xl">
                 <FilterSortTableWithPage dataI={data.combinadas} headers={headersComb} id={`ex-multi-results`} size={7}/>
               </div>
           </div>
